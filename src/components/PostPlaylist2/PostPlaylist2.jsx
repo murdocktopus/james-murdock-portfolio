3af54b82892e91e9a6826cccd67f3761cdee37dd
James Murdock <james.k.murdock@gmail.com>
2022-05-26 05:33:02 -0700

    playlist components, updated tab icon logo

--- src/components/PostPlaylist2/PostPlaylist2.jsx
+++ src/components/PostPlaylist2/PostPlaylist2.jsx
@@ -1,21 +1,25 @@
-import "./PostPlaylist1.scss";
+import "./PostPlaylist2.scss";
 import { Link, Route, Redirect } from "react-router-dom";
 import PostCard1 from "../PostCard1/PostCard1";
+import PostCard2 from "../PostCard2/PostCard2";
 
-function PostPlaylist1(props) {
+function PostPlaylist2(props) {
   return (
     <>
       <section className="post-playlist-section">
         <div className="playlist">
           <p className="playlist__title">{props.title}</p>
           <div className="playlist__list-container">
-            <PostCard1 />
-            <PostCard1 />
-            <PostCard1 />
+            <PostCard2 />
+            <PostCard2 />
+            <PostCard2 />
+            <PostCard2 />
+            <PostCard2 />
+            <PostCard2 />
           </div>
         </div>
       </section>
     </>
   );
 }
-export default PostPlaylist1;
+export default PostPlaylist2;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,16 @@ import "./HomePage.scss";
 import { Component } from "react";
 import Hero from "../../components/Hero/Hero";
 import PostPlaylist1 from "../../components/PostPlaylist1/PostPlaylist1";
+import PostPlaylist2 from "../../components/PostPlaylist2/PostPlaylist2";
 
 class HomePage extends Component {
   render() {
     return (
       <div className="home-page">
         <Hero />
-        <PostPlaylist1 />
+        <PostPlaylist1 title="Featured Today" />
+        <PostPlaylist2 title="Blog" />
+        <PostPlaylist2 title="Projects" />
       </div>
     );
   }
