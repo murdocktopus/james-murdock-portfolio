cf8cd89dbfbfb1bf6dfcf7a87aeac2a273e33c51
James Murdock <james.k.murdock@gmail.com>
2023-02-12 02:38:15 -0800

    Switched App.js from Class Based to Functional Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './styles/_global.scss';
-import { Component } from 'react';
+import { Component, useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -19,88 +19,39 @@ import Footer from './components/Footer/Footer';
 import axios from 'axios';
 import MenuModal from './components/MenuModal/MenuModal';
 
-class App extends Component {
-  state = {
-    allPosts: [],
-    allTags: [],
-    heroPosts: [],
-    selectedPost: {},
-    tagsAsPaths: [],
+function App() {
+  const [allPosts, setAllPosts] = useState([]);
+  const [allTags, setAllTags] = useState([]);
+  const [heroPosts, setHeroPosts] = useState([]);
+  const [tagsAsPaths, setTagsAsPaths] = useState([]);
+  const [selectedPost, setSelectedPost] = useState({});
+  const [openMenuModal, setOpenMenuModal] = useState(false);
 
-    // pagesAsPaths: [],
-    // searchTerm: '',
-    // searchSubmitted: false,
-  };
-
-  //
-  //
-  //
-  // Component Lifecycle Events (Below)
-  //
-  //
-  //
-
-  componentDidMount() {
-    this.getPosts();
-    // this.getSetTagsAndTagRoutes();
-  }
-
-  componentDidUpdate() {
-    if (this.state.tagsAsPaths === false) {
-      console.log(this.state.tagsAsPaths);
-      this.getSetTagsAndTagRoutes();
-    }
-    // console.log('App', 'this.state.heroPosts:', this.state.heroPosts);
-  }
-
-  //
-  //
-  //
-  // "Executive" Functions (Below)
-  //
-  //
-  //
-
-  // Immediate Functions (with Axios Request)
-  //
-  //
-
-  getPosts = () => {
+  useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/blog-posts`)
       .then((response) => {
-        // console.log('All Posts in API', response.data);
-        let allPosts = response.data;
-        this.setState({
-          allPosts: allPosts,
-        });
-        this.getSetTagsAndTagRoutes();
-        this.getSetHeroPosts();
+        setAllPosts(response.data);
       })
       .catch((err) => {
         console.log('error 1 catch', err);
       });
-  };
+  }, []);
 
-  getSetHeroPosts = () => {
-    const allPosts = this.state.allPosts;
+  useEffect(() => {
     const heroPosts = [
       allPosts.find((posts) => posts.hero === 1),
       allPosts.find((posts) => posts.hero === 2),
       allPosts.find((posts) => posts.hero === 3),
       allPosts.find((posts) => posts.hero === 1),
     ];
-    this.setState({
-      heroPosts: heroPosts,
-    });
-    // console.log(`heroposts`, heroPosts);
-  };
+    setHeroPosts(heroPosts);
+  }, []);
 
-  getSetTagsAndTagRoutes = () => {
+  useEffect(() => {
     // This Function takes all the different types of tags (tags, stack tags, skill tags)
     // and puts them into a single array, replaces capital letters and hyphens,
     // and sets the array as the state for allTags and tagsAsPaths (w/ ((tag) => `/tag/${tag}`))
-    const allPosts = this.state.allPosts;
     const postTags2DArr = allPosts && allPosts.map((post) => post.tags);
     const skillTags2DArr = allPosts && allPosts.map((post) => post.skillTags);
     const stackTags2DArr = allPosts && allPosts.map((post) => post.stackTags);
@@ -109,30 +60,20 @@ class App extends Component {
     const postTagsFormatted =
       allTags1DArr &&
       allTags1DArr.map((tag) => tag.replace(/\s+/g, '-').toLowerCase());
-    this.setState({
-      allTags: postTagsFormatted,
-    });
+    setAllTags(postTagsFormatted);
     const tagsAsPaths =
       postTagsFormatted && postTagsFormatted.map((tag) => `/tag/${tag}`);
-    this.setState({
-      tagsAsPaths: tagsAsPaths,
-    });
-  };
+    setTagsAsPaths(tagsAsPaths);
+  }, []);
 
-  // Secondary Exec Functions w/
-  //
-  //
-
-  getSetPost = () => {
+  useEffect(() => {
     // Finds the post thats name matches the URL and sets State as selectedPost
-    const foundPost = this.state.allPosts.find(
+    const foundPost = allPosts.find(
       (post) => post.name === window.location.pathname.slice(6)
     );
     console.log('foundPost', foundPost);
-    this.setState({
-      selectedPost: foundPost,
-    });
-  };
+    setSelectedPost(foundPost);
+  }, []);
 
   // getSetPages = () => {
   //   const allPosts = this.state.allPosts;
@@ -163,114 +104,104 @@ class App extends Component {
   //   e.target.reset();
   // };
 
-  render() {
-    const allPosts = this.state.allPosts;
-    const postTags2DArr = allPosts && allPosts.map((post) => post.tags);
-    const postTags1DArr = [].concat(...(postTags2DArr && postTags2DArr));
-    const skillTags2DArr = allPosts && allPosts.map((post) => post.skillTags);
-    const skillTags1DArr = [].concat(...(skillTags2DArr && skillTags2DArr));
-    const stackTags2DArr = allPosts && allPosts.map((post) => post.stackTags);
-    const stackTags1DArr = [].concat(...(stackTags2DArr && stackTags2DArr));
-    const allTags2DArr = postTags2DArr.concat(skillTags2DArr, stackTags2DArr);
-    const allTags1DArr = [].concat(...(allTags2DArr && allTags2DArr));
-    const postTagsFormatted =
-      allTags1DArr &&
-      allTags1DArr.map((tag) => tag.replace(/\s+/g, '-').toLowerCase());
+  // const allPosts = this.state.allPosts;
+  const postTags2DArr = allPosts && allPosts.map((post) => post.tags);
+  const postTags1DArr = [].concat(...(postTags2DArr && postTags2DArr));
+  const skillTags2DArr = allPosts && allPosts.map((post) => post.skillTags);
+  const skillTags1DArr = [].concat(...(skillTags2DArr && skillTags2DArr));
+  const stackTags2DArr = allPosts && allPosts.map((post) => post.stackTags);
+  const stackTags1DArr = [].concat(...(stackTags2DArr && stackTags2DArr));
+  const allTags2DArr = postTags2DArr.concat(skillTags2DArr, stackTags2DArr);
+  const allTags1DArr = [].concat(...(allTags2DArr && allTags2DArr));
+  const postTagsFormatted =
+    allTags1DArr &&
+    allTags1DArr.map((tag) => tag.replace(/\s+/g, '-').toLowerCase());
 
-    // const tagsAsPaths =
-    //   postTagsFormatted && postTagsFormatted.map((tag) => `/tag/${tag}`);
+  // const tagsAsPaths =
+  //   postTagsFormatted && postTagsFormatted.map((tag) => `/tag/${tag}`);
 
-    // const tagsAsRoutes =
-    //   postTagsFormatted &&
-    //   postTagsFormatted.map((tag) => (
-    //     <Route
-    //       path={`/tag/${tag}`}
-    //       element={
-    //         <TagFeedPage
-    //           allPosts={this.state.allPosts}
-    //           tagsAsPaths={this.state.tagsAsPaths}
-    //           allTags={this.state.allTags}
-    //         />
-    //       }
-    //     />
-    //   ));
+  // const tagsAsRoutes =
+  //   postTagsFormatted &&
+  //   postTagsFormatted.map((tag) => (
+  //     <Route
+  //       path={`/tag/${tag}`}
+  //       element={
+  //         <TagFeedPage
+  //           allPosts={this.state.allPosts}
+  //           tagsAsPaths={this.state.tagsAsPaths}
+  //           allTags={this.state.allTags}
+  //         />
+  //       }
+  //     />
+  //   ));
 
-    // const postsAsPaths = allPosts && allPosts.map((post) => `/${post.name}`);
-    // const postsAsRoutes =
-    //   allPosts &&
-    //   allPosts.map((post) => (
-    //     <Route
-    //       path={`/${post.name}`}
-    //       element={
-    //         <BlogPostPage
-    //           selectedPost={this.state.selectedPost}
-    //           getSetPost={this.getSetPost}
-    //         />
-    //       }
-    //     />
-    //   ));
+  // const postsAsPaths = allPosts && allPosts.map((post) => `/${post.name}`);
+  // const postsAsRoutes =
+  //   allPosts &&
+  //   allPosts.map((post) => (
+  //     <Route
+  //       path={`/${post.name}`}
+  //       element={
+  //         <BlogPostPage
+  //           selectedPost={this.state.selectedPost}
+  //           getSetPost={this.getSetPost}
+  //         />
+  //       }
+  //     />
+  //   ));
 
-    return (
-      <div className='App'>
-        <Router>
-          <Header />
-          <MenuModal
-            allTags={this.state.allTags}
-            postTagsFormatted={postTagsFormatted}
-            allTags1DArr={allTags1DArr}
-            postTags1DArr={postTags1DArr}
+  return (
+    <div className='App'>
+      <Router>
+        <Header />
+        <MenuModal
+          allTags={allTags}
+          postTagsFormatted={postTagsFormatted}
+          allTags1DArr={allTags1DArr}
+          postTags1DArr={postTags1DArr}
+        />
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage allPosts={allPosts} heroPosts={heroPosts} />}
           />
-          <Routes>
-            <Route
-              path='/'
-              element={
-                <HomePage
-                  allPosts={this.state.allPosts}
-                  heroPosts={this.state.heroPosts}
-                />
-              }
-            />
-            <Route
-              path='/blog'
-              element={<BlogFeedPage allPosts={this.state.allPosts} />}
-            />
-            <Route
-              path='/portfolio'
-              element={<PortfolioFeedPage allPosts={this.state.allPosts} />}
-            />
-            {postTagsFormatted &&
-              postTagsFormatted.map((tag) => (
-                <Route
-                  key={tag}
-                  path={`/tag/${tag}`}
-                  element={
-                    <TagFeedPage
-                      allPosts={this.state.allPosts}
-                      tagsAsPaths={this.state.tagsAsPaths}
-                      allTags={this.state.allTags}
-                    />
-                  }
-                />
-              ))}
-            {allPosts &&
-              allPosts.map((post) => (
-                <Route
-                  key={post.id}
-                  path={`post/:${post.name}`}
-                  element={
-                    <BlogPostPage
-                      selectedPost={this.state.selectedPost}
-                      getSetPost={this.getSetPost}
-                    />
-                  }
-                />
-              ))}
-          </Routes>
-          <Footer />
-        </Router>
-      </div>
-    );
-  }
+          <Route path='/blog' element={<BlogFeedPage allPosts={allPosts} />} />
+          <Route
+            path='/portfolio'
+            element={<PortfolioFeedPage allPosts={allPosts} />}
+          />
+          {postTagsFormatted &&
+            postTagsFormatted.map((tag) => (
+              <Route
+                key={tag}
+                path={`/tag/${tag}`}
+                element={
+                  <TagFeedPage
+                    allPosts={allPosts}
+                    tagsAsPaths={tagsAsPaths}
+                    allTags={allTags}
+                  />
+                }
+              />
+            ))}
+          {allPosts &&
+            allPosts.map((post) => (
+              <Route
+                key={post.id}
+                path={`post/:${post.name}`}
+                element={
+                  <BlogPostPage
+                    selectedPost={selectedPost}
+                    // getSetPost={this.getSetPost}
+                  />
+                }
+              />
+            ))}
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/MenuModal/MenuModal.jsx
+++ src/components/MenuModal/MenuModal.jsx
@@ -6,11 +6,34 @@ function MenuModal(props) {
   return (
     <div className='menu-modal-background'>
       <div className='menu-modal-container'>
-        <h2 className='menu-modal-header'>Menu</h2>
-        <p>Tags:</p>
-        {props.postTags1DArr.map((tag) => (
-          <p>{tag}</p>
-        ))}
+        <h2 className='menu-modal-title'>Menu</h2>
+        <div className='menu-modal-tags__title-container'>
+          <p className='menu-modal-tags__title'>General:</p>
+        </div>
+
+        <div className='menu-modal-tags__content-container'>
+          <ul>
+            <li>
+              <p className='menu-modal-tags__content'>Movies</p>
+            </li>
+            <li>
+              <p className='menu-modal-tags__content'>Games</p>
+            </li>
+            <li>
+              <p className='menu-modal-tags__content'>Books</p>
+            </li>
+            <li>
+              <p className='menu-modal-tags__content'>Sports</p>
+            </li>
+            <li>
+              <p className='menu-modal-tags__content'>Design</p>
+            </li>
+            <li>
+              <p className='menu-modal-tags__content'>Websites</p>
+            </li>
+          </ul>
+        </div>
+
         <div className='menu-modal-body'></div>
         <div className='menu-modal-footer'></div>
       </div>
